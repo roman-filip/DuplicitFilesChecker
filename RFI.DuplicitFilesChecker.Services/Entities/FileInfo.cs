b9ab2cec7531c1e21961568da77ebefee0fe1585
Roman Filip <dicky@email.cz>
2019-07-27 18:09:47 +0200

    Added entity for FileInfo

---
+++ RFI.DuplicitFilesChecker.Services/Entities/FileInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RFI.DuplicitFilesChecker.Services.Entities
+{
+    public class FileInfo
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Hash { get; set; }
+        public DateTime Date { get; set; }
+    }
+}

849f079b9a0b4a4525af6f60ebc22ea2e31fe60f
Roman FILIP <dicky@email.cz>
2019-08-09 19:10:18 +0200

    Added facade for export files info to JSON file

---
+++ RFI.DuplicitFilesChecker.Services/Facades/FileWriterFacade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using RFI.DuplicitFilesChecker.Services.Services;
+
+namespace RFI.DuplicitFilesChecker.Services.Facades
+{
+    public class FileWriterFacade
+    {
+        private readonly IFileInfoCalculationService _fileInfoCalculationService;
+
+        public FileWriterFacade(IFileInfoCalculationService fileInfoCalculationService)
+        {
+            _fileInfoCalculationService = fileInfoCalculationService;
+        }
+
+        public void WriteFilesInfoToFile(string inputDirectory, string outputFile)
+        {
+            if (File.Exists(outputFile))
+            {
+                throw new ApplicationException($"File '{outputFile}' already exists.");
+            }
+
+            var fileInfos = _fileInfoCalculationService.CalculateFileInfos(inputDirectory);
+            using (StreamWriter file = File.CreateText(outputFile))
+            {
+                JsonSerializer serializer = new JsonSerializer { Formatting = Formatting.Indented };
+                serializer.Serialize(file, fileInfos);
+            }
+        }
+    }
+}
